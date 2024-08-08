a18d001cf1b6f62701408edffce72057dd3e173c
Dinu Rusu <dinu@codingshadows.com>
2024-08-08 07:35:20 +0300

    updated matrix sum & product requirement

--- src/main/java/com/codingshadows/primitives/MatrixSumAndProduct.java
+++ src/main/java/com/codingshadows/primitives/MatrixSumAndProduct.java
@@ -6,8 +6,10 @@ public class MatrixSumAndProduct {
      * TODO:
      * Compute and print the sum matrix and the product matrix
      * between two square matrices of size n, that are read from the keyboard (in the console).
+     * The size, n, is also read from the keyboard.
      * You can see how the input is expected to look like in the test cases, but for a quick reference,
-     * it looks like this:
+     * it looks like this:  (1,2,3,4 is the first matrix; 5,6,7,8 is the second matrix)
+     * 2
      * 1 2
      * 3 4
      * 5 6
